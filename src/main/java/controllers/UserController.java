6a8cb4d5bd2b52bd3a950269cc0bbec8a762c9bb
aziz <aziz@10.10.27.51>
2021-10-06 22:12:41 +0200

    First Commit

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	// display list of users
+	// display list of user
 	@GetMapping("/")
 	public String viewHomePage(Model model) {
 		model.addAttribute("listUsers", userService.getAllUsers());
